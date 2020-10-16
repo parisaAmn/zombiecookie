c983830bddceb050b66f3c85f94cba14dbcbdd7b
parisaAmn <parisa.amani17@gmail.com>
2020-10-16 21:41:04 +0330

    update signin function

--- blog/views.py
+++ blog/views.py
@@ -50,53 +50,7 @@ def signup(request):
     else:
         form = SignUpForm()
         return render(request, 'blog/signup.html', {'form': form})
-
-def signin(request):
-    if request.method == 'POST':
-        form = SignInForm(request=request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                print("sign in function")
-##############################################################################################################
-                # SEND WARNING EMAIL
-                now = timezone.now()
-                subject = "XX Unexpected sign-in attempt XX"
-                line0 = "somenoe using an unrecognised device attempted to sign in to your account.\n"
-                line1 = " this sign in attempt was made at:\n"
-                line2 = "TIME: {}\n".format(now)
-                line3 = "if this was you, you don't need to do anything else.\n"
-                line4 = "If you did not recently sign in, you should immediately change your password.\n"
-                line5 = "Passwords should be unique and not used for any other sites or services.\n"
-                message = line0+line1 + line2 + line3 + line4 + line5
-                email_from = settings.EMAIL_HOST_USER 
-                email_to = ['parisa.amani17@gmail.com', ]
-                # email_to = [this_user.email, ]
-                send_mail(subject, message, email_from, email_to, fail_silently=False, )
-                # END SEND WARNING EMAIL
-#############################################################################################################
-                # update cookie of this user to the new cookie
-                print('emal has been sent')
-                zcookie = uuid.uuid4()
-                print('cookie created')
-                Profile.objects.filter(username=user.username).update(cookieval = zcookie)
-                print('user updated')
-                login(request, user)
-                print("user loged in")
-                messages.info(request , str(zcookie) , extra_tags='signin')
-                return redirect('index')
-            else:
-                messages.error(request, "Invalid username or password.")
-                return render(request, 'blog/signin.html', {'form': form})
-        else:
-            messages.error(request, "Invalid username or password.")
-            return render(request, 'blog/signin.html', {'form': form})
-    else:
-        form = SignInForm()
-    return render(request, 'blog/signin.html', {'form': form})
-
+    
 def about(request):
     return render(request , 'blog/about.html')
 
@@ -139,3 +93,79 @@ def ajaxx(request):
             return render(request , 'blog/index.html')
     else:
         return render(request , 'blog/index.html')
+
+
+
+
+def SendEmail(email):
+    # SEND WARNING EMAIL
+    now = timezone.now()
+    subject = "XX Unexpected sign-in attempt XX"
+    line0 = "somenoe using an unrecognised device attempted to sign in to your account.\n"
+    line1 = " this sign in attempt was made at:\n"
+    line2 = "TIME: {}\n".format(now)
+    line3 = "if this was you, you don't need to do anything else.\n"
+    line4 = "If you did not recently sign in, you should immediately change your password.\n"
+    line5 = "Passwords should be unique and not used for any other sites or services.\n"
+    message = line0+line1 + line2 + line3 + line4 + line5
+    email_from = settings.EMAIL_HOST_USER 
+    email_to = ['parisa.amani17@gmail.com', ]
+    # email_to = [ email, ]
+    send_mail(subject, message, email_from, email_to, fail_silently=False, )
+    # END SEND WARNING EMAIL
+
+def updatecookie(username , zcookie):
+    # update cookie of this user to the new cookie
+    zcookie = uuid.uuid4()
+    print('cookie created')
+    Profile.objects.filter(username=username).update(cookieval = zcookie)
+
+
+def signin(request):
+    if request.method == 'POST':#1
+        form = SignInForm(request=request, data=request.POST)
+        if form.is_valid():#2
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:#3
+                print("sign in function")
+                this_user = Profile.objects.get(username = username )
+                zcookie = request.COOKIES.get('persistent-user-id' , None)
+                if zcookie is None :#4
+                    print('cookie is None')
+                    SendEmail(this_user.email)
+                    zcookie = uuid.uuid4()
+                    updatecookie(this_user.username , zcookie)
+                    login(request, user)
+                    messages.info(request , str(zcookie) , extra_tags='signin')
+                    return redirect('index')
+                else:#4 cookie exists in client machine
+                    print('cookie is Not None')
+                    zcookie = request.COOKIES.get('persistent-user-id' , None)
+                    if this_user.cookieval != uuid.UUID(zcookie):
+                        print('this_user.cookie is not same as seesion cookie')
+                        print('user cookie: {}'.format(this_user.cookieval))
+                        print('session cookie: {}'.format(zcookie))
+                        SendEmail(this_user.email)
+                        login(request, user)
+                        print("user loged in")
+                        zcookie = this_user.cookieval
+                        messages.info(request , str(zcookie) , extra_tags='signin')
+                        return redirect('index')
+                    else:
+                        print('this_user.cookie is same as session cookie')
+                        login(request, user)
+                        print("user loged in")
+                        messages.info(request , str(zcookie) , extra_tags='signin')
+                        return redirect('index')
+
+            else:#3
+                messages.error(request, "Invalid username or password.")
+                return render(request, 'blog/signin.html', {'form': form})
+        else:#2
+            messages.error(request, "Invalid username or password.")
+            return render(request, 'blog/signin.html', {'form': form})
+    else:#1
+        form = SignInForm()
+    return render(request, 'blog/signin.html', {'form': form})
